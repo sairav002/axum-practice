a2e512c7c1605a3a0c8be1a167d20735a65b6d47
sairav002 <arivas00@protonmail.com>
2024-11-28 22:25:47 +0100

    some basic routes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
 #![allow(unused)]
 
-use axum::{response::{Html, IntoResponse}, routing::get, Router};
+use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::{get, get_service}, Router};
+use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(handler_hello),
-    );
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("Listening on {:?}\n", listener.local_addr());
-    axum::serve(listener, routes_hello);
+    axum::serve(listener, routes_all).await.unwrap();
+
+    //#[warn(while_true)]
+    //loop {};
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 
-async fn handler_hello() -> impl IntoResponse {
-    Html("Hello world!")
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("hello1");
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello {name}"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("hello2");
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
