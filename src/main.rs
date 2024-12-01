7d810f0b991c8a61dd1b603139cb438ed66353e3
sairav002 <arivas00@protonmail.com>
2024-12-01 12:21:42 +0100

    login auth token cookie

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub use self::errors::{Error, Result};
 
 use axum::{extract::{Path, Query}, middleware, response::{Html, IntoResponse, Response}, routing::{get, get_service}, Router};
 use serde::Deserialize;
+use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
 mod errors;
@@ -11,10 +12,12 @@ mod web;
 
 #[tokio::main]
 async fn main() {
+    // Layers executed from bottom to top
     let routes_all = Router::new()
         .merge(routes_hello())
         .merge(web::routes_login::routes())
         .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
--- src/web/routes_login.rs
+++ src/web/routes_login.rs
@@ -1,20 +1,21 @@
-use crate::{Error, Result};
+use crate::{web::AUTH_TOKEN, Error, Result};
 use serde::Deserialize;
 use axum::{routing::post, Json, Router};
 use serde_json::{json, Value};
+use tower_cookies::{Cookie, Cookies};
 
 pub fn routes() -> Router {
     Router::new().route("/api/login", post(api_login))
 }
 
-async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
+async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json<Value>> {
     println!("->> {:<12} - api_login", "HANDLER");
 
     if payload.username != "demo1" || payload.pwd != "welcome" {
         return Err(Error::LoginFail);
     }
 
-    // TODO: Set cookies
+    cookies.add(Cookie::new(AUTH_TOKEN, "user-1.exp.sign"));
 
     // Create the success body.
     let body = Json(json!({
